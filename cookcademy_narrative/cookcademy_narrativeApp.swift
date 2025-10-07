9370fd52ba447ca21adf36fb99a9d715d0fbdf54
priambodo.pangestu <priambodo.pangestu@primecash.co.id>
2025-10-07 16:39:06 +0700

    Initial Commit

---
+++ cookcademy_narrative/cookcademy_narrativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookcademy_narrativeApp.swift
+//  cookcademy_narrative
+//
+//  Created by Pangestu, Priambodo on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct cookcademy_narrativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
